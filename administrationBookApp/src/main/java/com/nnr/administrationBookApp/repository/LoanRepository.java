9be68d704a489ca4d93d8fd32847965d016fec9d
Roumuald Noubiap Nomsi <roumualdnoubiap@gmail.com>
2023-07-23 12:14:18 +0200

    chearch Loan and method interface Book

--- administrationBookApp/src/main/java/com/nnr/administrationBookApp/repository/LoanRepository.java
+++ administrationBookApp/src/main/java/com/nnr/administrationBookApp/repository/LoanRepository.java
@@ -1,11 +1,25 @@
 package com.nnr.administrationBookApp.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.nnr.administrationBookApp.enumerate.LoanStatus;
 import com.nnr.administrationBookApp.model.Loan;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long>{
+	
+	public List<Loan> findByEndDateBefore(Date maxEndDate);
+    
+    @Query("SELECT lo "
+         + "FROM Loan lo "
+         + "INNER JOIN lo.pk.customer c "
+         + "WHERE UPPER(c.email) = UPPER(?1) "
+         + "   AND lo.status = ?2 ")
+    public List<Loan> getAllOpenLoansOfThisCustomer(String email, LoanStatus status);
 
 }
--- administrationBookApp/src/main/java/com/nnr/administrationBookApp/service/InterfaceBook.java
+++ administrationBookApp/src/main/java/com/nnr/administrationBookApp/service/InterfaceBook.java
@@ -1,5 +1,23 @@
 package com.nnr.administrationBookApp.service;
 
+import java.util.List;
+
+import com.nnr.administrationBookApp.model.Book;
+
 public interface InterfaceBook {
+	
+	public Book saveBook(Book book, Long categoryId);
+    
+    public Book updateBook(Book book);
+    
+    public void deleteBook(Integer bookId);
+    
+    public List<Book> findBooksByTitleOrPartTitle(String title);
+    
+    public Book findBookByIsbn(String isbn);
+    
+    public boolean checkIfIdexists(Integer id);
+    
+    public List<Book> getBooksByCategory(String codeCategory);
 
 }
